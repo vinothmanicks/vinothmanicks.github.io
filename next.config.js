23a88e7c36224a8b91041c64e3fc0185c415cd59
Vinoth Kumar Manickavasagam <vmv@amazon.com>
2023-12-24 22:30:42 -0800

    Change github workflow and next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    poweredByHeader: false,
+};
 
 module.exports = nextConfig;
